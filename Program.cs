66eb26c131e6ee1f24f79f85991a0a91c5654d90
James Ferguson <jimmiexd@gmail.com>
2020-08-29 21:00:17 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OptfineInstaller {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                System.Diagnostics.Process.Start("CMD.exe", $"/C \"C:\\Program Files (x86)\\Minecraft Launcher\\runtime\\jre-x64\\bin\\java.exe\" -jar {args[0]}");
+            }
+            catch(System.Exception ex) {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
